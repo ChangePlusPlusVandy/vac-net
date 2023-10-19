28d1fb8a66384a979a9619bbaa07fa321e5e8dba
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-10-19 14:38:25 -0700

    add model and controller example

---
+++ server/src/controllers/exampleusers.controllers.ts
@@ -0,0 +1,9 @@
+import User from "../models/ExampleUser";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { auth } from "../../config/firebase";
+
+const getUsers = async (req: Request, res: Response): Promise<any> => {
+  // Define your endpoint logic here
+};
+
+export { getUsers };
