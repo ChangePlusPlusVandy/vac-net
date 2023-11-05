e6f7f062835a91adb6d55a050d1ffc26b4ee981a
atifmomin <atifmomin12345@gmail.com>
2023-11-05 13:29:02 -0600

    loan model

---
+++ server/src/models/OutstandingLoan.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+interface Loan {
+  initialPayment?: number;
+  initialPaymentDate?: Date;
+  principalLeft?: number;
+  nextPaymentDate?: Date;
+  nextPaymentAmount?: number;
+  archivedLoan?: boolean;
+  beneficiary?: string;
+  validLoan?: boolean;
+}
+
+const LoanSchema = new mongoose.Schema<Loan>({
+    initialPayment: {
+      type: Number,
+    },
+    initialPaymentDate: {
+      type: Date,
+      default: Date.now,
+    // You can add required: true if needed
+    },
+    principalLeft: {
+      type: Number,
+    },
+    nextPaymentDate: {
+      type: Date,
+    // You can add default values or required: true if needed
+    },
+    nextPaymentAmount: {
+      type: Number,
+    },
+    archivedLoan: {
+      type: Boolean,
+      default: false,
+    },
+    beneficiary: {
+      type: String,
+    },
+    validLoan: {
+      type: Boolean,
+    },
+  });
+  
+  export default mongoose.model<Loan>("OutstandingLoan", LoanSchema);
