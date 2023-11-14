90853911375b65842c3dc57194daf77816540d8e
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-11-14 16:05:15 -0600

    format

--- server/src/routes/loansRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { editLoan, deleteLoan } from "../controllers/loans.controllers";
-
-const router = express.Router();
-router.put("/", editLoan);
-router.delete("/:_id", deleteLoan);
-
-export default router;
