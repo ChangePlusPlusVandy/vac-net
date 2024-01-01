4e6a15d1d121c84fe1d8bcc3b12ed6bad45969a5
Raj Chopra <raj.chopra@vanderbilt.edu>
2024-01-01 16:02:26 -0700

    add shadcn ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       // Proxy /api requests to our express server
