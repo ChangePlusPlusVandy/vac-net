640f179e9571f75c30941b1e8793b49063c47b50
Isaac Liu <xnscdev@gmail.com>
2024-01-23 00:26:51 -0600

    Editable staff member page

--- client/src/pages/staff/staff.tsx
+++ client/src/pages/staff/staff.tsx
@@ -5,6 +5,9 @@ import { DashboardHeader } from "@/components/header";
 import { DashboardShell } from "@/components/shell";
 import { ItemCreateButton } from "@/components/create-item-button";
 import StaffToolbar from "@/components/toolbars/staff-toolbar";
+import { buttonVariants } from "@/components/ui/button";
+import { Icons } from "@/components/ui/icons";
+import { cn } from "@/lib/utils";
 import type { IStaff } from "@/pages/staff/staff-members";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
@@ -19,98 +22,135 @@ const Staff = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [editing, setEditing] = useState(false);
 
+  const handleSaveStaff = async () => {
+    if (params.get("f") === "1") {
+      setIsLoading(true);
+      try {
+        await fetch(
+          `http://localhost:3001/user/edit?_id=${staff?._id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(staff),
+          },
+        ).then((res: Response) => res.json() as unknown as IStaff);
+        setEditing(false);
+        setParams({ f: "0" });
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      setParams({ f: "1" });
+      setEditing(true);
+    }
+  };
+
   useEffect(() => {
     const getStaffById = async () => {
       setIsLoading(true);
-      try{
-        const data : IStaff = await fetch(
-          "http://localhost:3001/user/getstaff?staffId=" + id
+      try {
+        const data: IStaff = await fetch(
+          "http://localhost:3001/user/getstaff?staffId=" + id,
         ).then((res: Response) => res.json() as unknown as IStaff);
-
         setStaff(data);
-      }catch(e){
+      } catch (e) {
         console.log(e);
-      }finally{
+      } finally {
         setIsLoading(false);
       }
-    }
+    };
 
     void getStaffById();
   }, [id, editing]);
 
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStaff({ ...staff, firstName: e.target.value });
+  };
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStaff({ ...staff, lastName: e.target.value });
+  };
+
+  const handleStatusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStaff({ ...staff, status: e.target.value });
+  };
+
+  const handleClearanceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStaff({ ...staff, clearance: e.target.value });
+  };
 
   return (
     <DashboardShell>
       <DashboardHeader
         heading="Staff"
-        text={"View and edit " + staff?.firstName + " " + staff?.lastName + "'s data."}
+        text={
+          "View and edit " +
+          staff?.firstName +
+          " " +
+          staff?.lastName +
+          "'s data."
+        }
       >
-        <ItemCreateButton item="Onboard New Member" />
+        <SaveStaff
+            isLoading={isLoading}
+            editing={params.get("f") === "1"}
+            onClick={handleSaveStaff}
+          />
       </DashboardHeader>
       <div className="grid gap-4 py-4">
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-left">
-              First Name
+          <Label htmlFor="fname" className="text-left">
+            First Name
           </Label>
           <Input
             id="fname"
             className="col-span-1"
             value={staff?.firstName}
-            // onChange={handleFirstNameChange}
-            disabled={true}
+            onChange={handleFirstNameChange}
+            disabled={params.get("f") !== "1"}
           />
         </div>
 
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-left">
+          <Label htmlFor="lname" className="text-left">
             Last Name
           </Label>
           <Input
             id="lname"
             className="col-span-1 pr-1"
             value={staff?.lastName}
-            // onChange={handleLastNameChange}
-            disabled={true}
+            onChange={handleLastNameChange}
+            disabled={params.get("f") !== "1"}
           />
         </div>
-
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-left">
-            Firebase UID
-          </Label>
-          <Input
-            id="firebaseUID"
-            className="col-span-1 pr-1"
-            value={staff?.firebaseUID}
-            // onChange={handleLastNameChange}
-            disabled={true}
-          />
-        </div>
+        
         {/* TODO: Display the date */}
 
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-left">
+          <Label htmlFor="status" className="text-left">
             Status
           </Label>
           <Input
             id="status"
             className="col-span-1 pr-1"
             value={staff?.status}
-            // onChange={handleLastNameChange}
-            disabled={true}
+            onChange={handleStatusChange}
+            disabled={params.get("f") !== "1"}
           />
         </div>
 
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-left">
+          <Label htmlFor="clearance" className="text-left">
             Clearance
           </Label>
           <Input
             id="clearance"
             className="col-span-1 pr-1"
             value={staff?.clearance}
-            // onChange={handleLastNameChange}
-            disabled={true}
+            onChange={handleClearanceChange}
+            disabled={params.get("f") !== "1"}
           />
         </div>
 
@@ -122,3 +162,40 @@ const Staff = () => {
 };
 
 export default Staff;
+
+const SaveStaff = ({
+  isLoading,
+  editing,
+  onClick,
+}: {
+  isLoading: boolean;
+  editing: boolean;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      className={cn(
+        buttonVariants({}),
+        {
+          "cursor-not-allowed opacity-60": isLoading,
+        },
+        "mr-4",
+      )}
+      disabled={isLoading}
+      onClick={onClick}
+    >
+      {isLoading ? (
+        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <>
+          {editing ? (
+            <Icons.save className="mr-2 h-4 w-4" />
+          ) : (
+            <Icons.edit className="mr-2 h-4 w-4" />
+          )}
+        </>
+      )}
+      {editing ? "Save Changes" : "Edit Staff Member"}
+    </button>
+  );
+};
